34f2377b4c340911b9d9a1f3a20286d2c3249f1a
Mursal Hajiyev <mursalhaciyev@gmail.com>
2024-01-14 15:41:48 +0100

    created new class and added the miles conversion

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class RewardValue {
+//public class RewardValue {
+//}
+
+//getMilesValue()
+
+//getCashValue()
+
+//RewardValue must convert from miles to cash at a rate of 0.0035
+
+//----
+
+//-----
+
+public class RewardValue {
+    private static final double MILES_TO_CASH_RATE = 0.0035;
+    private double cashValue;
+    private int milesValue;
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / MILES_TO_CASH_RATE);
+    }
+
+    // Constructor that accepts a miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_RATE;
+    }
+
+    // Getter method for cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Getter method for miles value
+    public int getMilesValue() {
+        return milesValue;
+    }
 }
