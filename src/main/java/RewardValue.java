a2651fb266180084bf9aa5928a6e62fcfafcd518
Mursal Hajiyev <mursalhaciyev@gmail.com>
2024-01-14 16:16:21 +0100

    the updated version with the true example code

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,40 +1,28 @@
-//public class RewardValue {
-//}
-
-//getMilesValue()
-
-//getCashValue()
-
-//RewardValue must convert from miles to cash at a rate of 0.0035
-
-//----
-
-//-----
-
 public class RewardValue {
-    private static final double MILES_TO_CASH_RATE = 0.0035;
-    private double cashValue;
-    private int milesValue;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-    // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = (int) (cashValue / MILES_TO_CASH_RATE);
     }
 
-    // Constructor that accepts a miles value
     public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
-        this.cashValue = milesValue * MILES_TO_CASH_RATE;
+        this.cashValue = convertToCash(milesValue);
     }
 
-    // Getter method for cash value
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
     public double getCashValue() {
         return cashValue;
     }
 
-    // Getter method for miles value
     public int getMilesValue() {
-        return milesValue;
+        return convertToMiles(this.cashValue);
     }
 }
